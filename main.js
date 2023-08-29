9c821b38ec6df0f356a16898d4e26c63ba611800
chaukighat2 <142743044+chaukighat2@users.noreply.github.com>
2023-08-29 22:42:05 +0545

    Update main.js

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 
 'use strict';
 
-const videoElement = document.querySelector('video');
+const videoElement = document.querySelector('local-video');
 const audioInputSelect = document.querySelector('select#audioSource');
 const audioOutputSelect = document.querySelector('select#audioOutput');
 const videoSelect = document.querySelector('select#videoSource');
